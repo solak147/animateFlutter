b799c765dcff5d08a6cb1ce0aa45e4ccfc760300
kevin.tong <kevin.tong@wh8office.com>
2022-12-26 00:43:52 +0800

    format

--- animate/lib/login/login_form.dart
+++ animate/lib/login/login_form.dart
@@ -9,6 +9,7 @@ import 'package:flutter_auth_buttons/flutter_auth_buttons.dart';
 
 class LoginForm extends StatefulWidget {
   final UserRepository _userRepository;
+
   LoginForm({Key key, @required UserRepository userRepository})
       : assert(userRepository != null),
         _userRepository = userRepository,
@@ -16,7 +17,6 @@ class LoginForm extends StatefulWidget {
 
   @override
   _LoginFormState createState() => _LoginFormState();
-
 }
 
 class _LoginFormState extends State<LoginForm> {
@@ -26,8 +26,10 @@ class _LoginFormState extends State<LoginForm> {
   bool _rememberAccount;
 
   UserRepository get _userRepository => widget._userRepository;
+
   bool get isPopulated =>
       _emailController.text.isNotEmpty && _passwordController.text.isNotEmpty;
+
   bool isLoginButtonEnabled(LoginState state) {
     return state.isFormValid && isPopulated && !state.isSubmitting;
   }
@@ -45,126 +47,127 @@ class _LoginFormState extends State<LoginForm> {
   @override
   Widget build(BuildContext context) {
     return BlocListener<LoginBloc, LoginState>(
-      listener: (BuildContext context, LoginState state) {
-        if (state.isFailure) {
-          Scaffold.of(context)
-            ..hideCurrentSnackBar()
-            ..showSnackBar(SnackBar(
-              content: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[Text('Login Failure'), Icon(Icons.error)],
-              ),
-              backgroundColor: Colors.red,
-            ));
-        }
-        if (state.isSubmitting) {
-          Scaffold.of(context)
-            ..hideCurrentSnackBar()
-            ..showSnackBar(SnackBar(
+        listener: (BuildContext context, LoginState state) {
+          if (state.isFailure) {
+            Scaffold.of(context)
+              ..hideCurrentSnackBar()
+              ..showSnackBar(SnackBar(
                 content: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Text('Logging In...'),
-                    CircularProgressIndicator(),
-                  ],
-                )));
-        }
-        if (state.isSuccess) {
-          if(_rememberAccount)
-            _saveAccount(_emailController.text);
-          BlocProvider.of<AuthenticationBloc>(context).dispatch(LoggedIn());
-        }
-      },
-      child: Scaffold(
-        appBar: AppBar(
-          actions: [
-            GestureDetector(
-              onTap: () =>Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => IgHome())),
-              child : Padding(
-                padding: const EdgeInsets.only(right : 180.0),
-                child: IconButton(
-                  icon: Icon(
-                    Icons.photo_camera_outlined,
-                    color: Colors.white,
-                  ),
-                  //onPressed: () => print('按下camera'),
-                  )
+                  children: <Widget>[Text('Login Failure'), Icon(Icons.error)],
                 ),
-            )],
-          ),
-        body: BlocBuilder<LoginBloc, LoginState>(
-        builder: (BuildContext context, LoginState state) {
-          return Padding(
-              padding: EdgeInsets.all(20.0),
-              child: Form(child: Material(
-                child: ListView(
-                  children: <Widget>[
-                    Padding(
-                      padding: EdgeInsets.symmetric(vertical: 20.0),
-                      child: Image.asset(
-                        'assets/logo.png',
-                        height: 200,
-                      ),
-                    ),
-                    TextFormField(
-                      controller: _emailController,
-                      decoration: InputDecoration(
-                          icon: Icon(Icons.email), labelText: 'Email'),
-                      autovalidate: true,
-                      autocorrect: false,
-                      validator: (_) {
-                        return !state.isEmailValid ? 'Invalid Email' : null;
-                      },
-                    ),
-                    TextFormField(
-                      controller: _passwordController,
-                      decoration: InputDecoration(
-                          icon: Icon(Icons.lock), labelText: 'Password'),
-                      obscureText: true,
-                      autovalidate: true,
-                      autocorrect: false,
-                      validator: (_) {
-                        return !state.isPasswordValid ? 'Invalid Password' : null;
-                      },
-                    ),
-                    CheckboxListTile(
-                      value: _rememberAccount ?? false,
-                      onChanged: (value){
-                        setState(() {
-                          _rememberAccount = value;
-                        });
-                        _saveCheckout(value);
-                      },
-                      title: Text('記住我的帳號'),
-                      controlAffinity: ListTileControlAffinity.leading,
-                    ),
-                    Padding(
-                      padding: EdgeInsets.symmetric(vertical: 20.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: <Widget>[
-                          LoginButton(
-                              onPressed: isLoginButtonEnabled(state)
-                                  ? _onFormSubmitted
-                                  : null),
-
-                          GoogleSignInButton(
-                            onPressed: () =>
-                                _loginBloc.dispatch(LoginWithGooglePressed()),
-                          ),
-                          CreateAccountButton(userRepository: _userRepository)
-                        ],
-                      ),
-                    )
-                  ],
-                ),
+                backgroundColor: Colors.red,
+              ));
+          }
+          if (state.isSubmitting) {
+            Scaffold.of(context)
+              ..hideCurrentSnackBar()
+              ..showSnackBar(SnackBar(
+                  content: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text('Logging In...'),
+                  CircularProgressIndicator(),
+                ],
               )));
-        }),
-      )
-    );
+          }
+          if (state.isSuccess) {
+            if (_rememberAccount) _saveAccount(_emailController.text);
+            BlocProvider.of<AuthenticationBloc>(context).dispatch(LoggedIn());
+          }
+        },
+        child: Scaffold(
+          appBar: AppBar(
+            actions: [
+              GestureDetector(
+                onTap: () => Navigator.push(
+                    context, MaterialPageRoute(builder: (context) => IgHome())),
+                child: Padding(
+                    padding: const EdgeInsets.only(right: 180.0),
+                    child: IconButton(
+                      icon: Icon(
+                        Icons.photo_camera_outlined,
+                        color: Colors.white,
+                      ),
+                      //onPressed: () => print('按下camera'),
+                    )),
+              )
+            ],
+          ),
+          body: BlocBuilder<LoginBloc, LoginState>(
+              builder: (BuildContext context, LoginState state) {
+              return Padding(
+                padding: EdgeInsets.all(20.0),
+                child: Form(
+                  child: Material(
+                    child: ListView(
+                      children: <Widget>[
+                        Padding(
+                          padding: EdgeInsets.symmetric(vertical: 20.0),
+                          child: Image.asset(
+                            'assets/logo.png',
+                            height: 200,
+                          ),
+                        ),
+                        TextFormField(
+                          controller: _emailController,
+                          decoration: InputDecoration(
+                              icon: Icon(Icons.email), labelText: 'Email'),
+                          autovalidate: true,
+                          autocorrect: false,
+                          validator: (_) {
+                            return !state.isEmailValid ? 'Invalid Email' : null;
+                          },
+                        ),
+                        TextFormField(
+                          controller: _passwordController,
+                          decoration: InputDecoration(
+                              icon: Icon(Icons.lock), labelText: 'Password'),
+                          obscureText: true,
+                          autovalidate: true,
+                          autocorrect: false,
+                          validator: (_) {
+                            return !state.isPasswordValid
+                                ? 'Invalid Password'
+                                : null;
+                          },
+                        ),
+                        CheckboxListTile(
+                          value: _rememberAccount ?? false,
+                          onChanged: (value) {
+                            setState(() {
+                              _rememberAccount = value;
+                            });
+                            _saveCheckout(value);
+                          },
+                          title: Text('記住我的帳號'),
+                          controlAffinity: ListTileControlAffinity.leading,
+                        ),
+                        Padding(
+                          padding: EdgeInsets.symmetric(vertical: 20.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            children: <Widget>[
+                              LoginButton(
+                                  onPressed: isLoginButtonEnabled(state)
+                                      ? _onFormSubmitted
+                                      : null),
+                              GoogleSignInButton(
+                                onPressed: () =>
+                                    _loginBloc.dispatch(LoginWithGooglePressed()),
+                              ),
+                              CreateAccountButton(userRepository: _userRepository)
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  )
+                )
+              );
+            }
+          ),
+        ));
   }
 
   @override
@@ -187,7 +190,7 @@ class _LoginFormState extends State<LoginForm> {
         email: _emailController.text, password: _passwordController.text));
   }
 
-  _loadCheckbox() async{
+  _loadCheckbox() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     setState(() {
       _rememberAccount = prefs.getBool('remember_account') ?? false;
